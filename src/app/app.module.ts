00bf909752bd108f1b71d91066641194d4d4183e
ANDREPC\Andre <andremarchezini@gmail.com>
2019-06-10 09:06:28 +0800

    added missing imports to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,15 @@ import * as Sentry from '@sentry/browser';
 import { AlertProvider } from './providers/alert/alert';
 import { AuthenticationProvider } from './providers/authentication/authentication';
 import { FormProvider } from './providers/form/form';
+import { ConnectProvider } from './providers/connect/connect';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 const isDevModeOn = isDevMode();
 
 Sentry.init({
   // ****************** REPLACE DSN
-  dsn: 'https://c75cf7e4e58e4d88bdc8e53e8d03f399@sentry.io/1471077'
+  // dsn: 'https://c75cf7e4e58e4d88bdc8e53e8d03f399@sentry.io/1471077'
 });
 
 @Injectable()
@@ -33,12 +36,22 @@ export class SentryErrorHandler implements ErrorHandler {
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot({
+      name: '_BoilerPlate',
+      driverOrder: ['sqlite', 'indexeddb', 'websql', 'localstorage'],
+    })
+  ],
   providers: [
     { provide: ErrorHandler, useClass: isDevModeOn ? ErrorHandler : SentryErrorHandler },
     AlertProvider,
     AuthenticationProvider,
     FormProvider,
+    ConnectProvider,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
